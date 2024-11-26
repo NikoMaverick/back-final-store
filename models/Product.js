c0601bf6bc720e1e87de09ea1aa8758a6c689d44
Niko Maverick <maverick.nk@gmail.com>
2024-11-26 19:09:41 +0100

    modelo schema ok

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    team: String,
+    year: String,
+    description: String,
+    set: String,
+    country: String,
+    league: String,
+    image: String,
+    size: String,
+    price: Number,
+}, {timestamps: true})
+
+const Product = mongoose.model('Product', ProductSchema)
+
+module.exports = Product
